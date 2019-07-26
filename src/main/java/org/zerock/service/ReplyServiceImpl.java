37fbf7ec9f285c6023feac931eb3b74fbf717d7d
okkosk10 <okkosk10@gmail.com>
2019-07-26 15:10:27 +0900

    파일업로드 다운로드

--- src/main/java/org/zerock/service/ReplyServiceImpl.java
+++ src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyPageDTO;
 import org.zerock.domain.ReplyVO;
 import org.zerock.mapper.ReplyMapper;
+import org.zerock.mapper.BoardMapper;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -19,10 +21,16 @@ public class ReplyServiceImpl implements ReplyService {
 	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
 	
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper boardMapper;
+	
+	@Transactional
 	@Override	
 	public int register(ReplyVO vo) {
 		
 		log.info("register......" + vo);
+		
+		boardMapper.updateReplyCnt(vo.getBno(), 1);
 		
 		return mapper.insert(vo);
 	}
@@ -43,11 +51,15 @@ public class ReplyServiceImpl implements ReplyService {
 		return mapper.update(vo);
 	}
 	
+	@Transactional
 	@Override
 	public int remove(Long rno) {
 		
 		log.info("remove....." + rno);
 		
+		ReplyVO vo = mapper.read(rno);
+		
+		boardMapper.updateReplyCnt(vo.getBno(), -1);
 		return mapper.delete(rno);
 	}
 	
--- src/main/webapp/WEB-INF/views/board/list.jsp
+++ src/main/webapp/WEB-INF/views/board/list.jsp
@@ -37,8 +37,8 @@
                                 		<td><c:out value="${board.bno}" /></td>
                     					<td>	
                     						<a class='move' href='<c:out value="${board.bno}"/>'>
-                    						<c:out value="${board.title}" /></a>
-                    					</td>
+                    						<c:out value="${board.title}" /> <b>[ <c:out value="${board.replyCnt}" /> ]</b>
+                    						</a>
                                 		<td><c:out value="${board.writer}" /></td>
                                 		<td><fmt:formatDate pattern = "yyyy-MM-dd" value = "${board.regdate }"/></td>
                                 		<td><fmt:formatDate pattern="yyyy-MM-dd"
